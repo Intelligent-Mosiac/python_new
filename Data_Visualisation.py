6f686d9dc2963f87ba103483a8245c303ca8c11d
Intelligent-Mosiac <62437360+Intelligent-Mosiac@users.noreply.github.com>
2020-04-11 22:54:20 +0530

    Update Data_Visualisation.py

--- Data_Visualisation.py
+++ Data_Visualisation.py
@@ -13,7 +13,8 @@ plt.show()
 #For subplot creation
 x=[i for i in range(10)]
 y=[i*i for i in range(10,20)]
-z=[i*2 for i in range(10,20)]plt.subplot(2,1,1)
+z=[i*2 for i in range(10,20)]
+plt.subplot(2,1,1)
 plt.plot(x,y)
 plt.subplot(2,1,2)
 plt.plot(x,z)
@@ -35,10 +36,11 @@ plt.bar(x,y,color="Blue",width=0.2,label="2017")
 plt.bar(x2,z,color="Red",width=0.3,label="2018")
 plt.legend()#Important to show labels
 plt.title("Bar Graph")
-plt.show()'''
+plt.show()
 
 #plt.pie(sizes,colors,labels,shadow,explode) Format for pie graph
 #Example is day distribution
+
 hours=[2,3,4,5,6,7,8]
 activities=["Youtbe","Chill","Fun","play","tv","rest","sleep"]
 explodes=[0,0.2,0,0,0,0,0.2]
