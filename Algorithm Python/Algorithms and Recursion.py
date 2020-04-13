7eec832b65d40e970cc37567a88f6b2becc1627e
Intelligent-Mosiac <62437360+Intelligent-Mosiac@users.noreply.github.com>
2020-04-13 22:53:57 +0530

    Update Algorithms and Recursion.py

--- Algorithm Python/Algorithms and Recursion.py
+++ Algorithm Python/Algorithms and Recursion.py
@@ -5,7 +5,7 @@ for i in range(len(a)):
     for j in range(1,len(a)):
         if a[j-1] > a[j]:
             count+=1
-            a[j-1],a[j]=a[j],a[j-1]# Swapping of 2 variable 1 linear
+            a[j-1],a[j]=a[j],a[j-1]# Swapping of 2 variable 1 line is mecessary and not a "style"
 print(a,count)
 #Insertion Sort   
 a=[6,5,3,1,8,7,2,4]
