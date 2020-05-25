2a9edb82c69f861b09645f86f03d5c9f873f21bc
Intelligent-Mosiac <62437360+Intelligent-Mosiac@users.noreply.github.com>
2020-05-25 22:24:30 +0530

    More Changes

---
+++ CBSE Python/3. Algorithms and Data Structures/Data Structrues/List.py
@@ -0,0 +1,30 @@
+"""Few useful properties of List
+1.Lists need not be homogenous,can contain strings,integers...
+2.They are mutuable and can be altered
+3.The elements in a List are indexed
+4.Elements of list can be duplicated
+5.They can be easily created without calling a function """
+
+List=[]
+print("Blank List:")
+print(List)
+
+List=[10,20,14]
+print("\nList of numbers :")
+print(List)
+
+# A Multidimensional List
+List=[['Geeks','For'],['Geeks']]
+print('\nMulti-Dimensional List')
+print(List)
+
+#Creating a List with Duplicate number
+List1=[1,7,8,9,54,6,7,7,8,8,8]
+print("\nList with the use of Numbers: ") 
+print(List1)
+
+# Size of a List
+List2=[10,2,3,4]
+print(len(List2))
+
+#Append of List
--- Python_External/Learn Python The HARD WAY/Intermediate/Array.py
+++
@@ -1,6 +0,0 @@
-"""Arrray"""
-import array
-
-arr=array.array('i',[1,2,3])
-
-print("The new created array is :")
