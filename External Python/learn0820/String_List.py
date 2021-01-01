281892452b1e1e2823bccd9d37551b037972d44b
Intelligent-Mosiac <62437360+Intelligent-Mosiac@users.noreply.github.com>
2021-01-01 08:01:24 +0530

    Folder Changes

--- External Python/learn0820/String_List.py
+++
@@ -1,8 +0,0 @@
-def split_and_join(line):
-   def split_and_join(line):
-   return("-".join(line.split()))
-
-if __name__ == '__main__':
-    line = input('this is a string')
-    result = split_and_join(line)
-    print(result)
